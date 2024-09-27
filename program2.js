d7da60840cd1d838d7838cc4c59c11e4bfcf2d9d
Rahul Singh <rahulsingh14062003@gmail.com>
2024-09-27 14:02:04 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,9 +2,45 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    
-};
-
+function romanToInt(s) {
+    const romanNumerals = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+    };
+  
+    let result = 0;
+    let prevValue = 0;
+  
+    for (let i = s.length - 1; i >= 0; i--) {
+      const currentValue = romanNumerals[s[i]];
+  
+      if (currentValue < prevValue) {
+        result -= currentValue;
+      } else {
+        result += currentValue;
+      }
+  
+      prevValue = currentValue;
+    }
+  
+    return result;
+  }
+  
+  // Test cases
+  const testCases = [
+    "III",
+    "LVIII",
+    "MCMXCIV",
+  ];
+  
+  for (let testCase of testCases) {
+    const result = romanToInt(testCase);
+    console.log(Input: ${testCase}, Output: ${result});
+  }
 
 module.exports={romanToInt}
